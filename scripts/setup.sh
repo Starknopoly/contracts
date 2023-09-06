94664fd383057777c8d49a513e4573749702036b
huaigu <wangchao@msn.cn>
2023-09-06 12:03:34 +0800

    chore: add sleep in setup.bash to avoid contract error

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,12 @@
 # todo: config file
 sozo execute set_townhall # 国库
+sleep 1
 sozo execute set_building --calldata 1,100 #酒店 100
+sleep 1
 sozo execute set_building --calldata 2,500 #银行 500
+sleep 1
 sozo execute set_building --calldata 3,1000 #加油站 1000
+sleep 1
 
 # Authoritarian
 sozo auth writer Player spawn
