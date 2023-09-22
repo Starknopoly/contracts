34981ae8ee77595c8251fc795cb2ed637d431280
huaigu <wangchao@msn.cn>
2023-09-22 11:48:27 +0800

    chore: update torii script

--- scripts/run_torii.sh
+++ scripts/run_torii.sh
@@ -1,8 +1,14 @@
-#Check if "dump-state.bin" file exists in the current directory
+#deploy contracts
+cd ../
+sozo --offline build
+sozo migrate --name test
+bash ./scripts/setup.sh
+
+#run torii
 if [ -e "indexer.db" ]; then
     echo "Deleting db file..."
     rm "indexer.db"
     echo "Deletion successful!"
 fi
 touch indexer.db
-torii --database-url indexer.db
+torii --database-url indexer.db --world 0x84486b8e9ffe38978b33c9d7685d9d2d487d0e9f096a1d2669edefc8506c35
