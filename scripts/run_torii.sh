24e25255ed289234f37817b95095621d6edc22fb
huaigu <wangchao@msn.cn>
2023-09-23 18:05:10 +0800

    chore: update scripts

--- scripts/run_torii.sh
+++ scripts/run_torii.sh
@@ -1,5 +1,18 @@
 #deploy contracts
-cd ../
+
+if [ "$1" == "deploy" ]; then
+  echo "build and deploy/upgrade contracts"
+  sozo --offline build
+  sozo migrate --name test
+  bash ./scripts/setup.sh
+  exit 0
+  # 在这里执行 option1 相关的逻辑
+elif [ "$1" == "server" ]; then
+  echo "Run torii server"
+  torii --database-url indexer.db --world 0x84486b8e9ffe38978b33c9d7685d9d2d487d0e9f096a1d2669edefc8506c35
+  exit 0
+fi
+
 sozo --offline build
 sozo migrate --name test
 bash ./scripts/setup.sh
